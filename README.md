febac6f15304d9dfcdf7e909232b3a07c9516ac9
andreyhitoshi1997 <andreyhitoshi1997@gmail.com>
2025-10-15 19:04:17 -0300

    subindo /health

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # clone-tabnews
 
+<<<<<<< HEAD
 Projeto do tabnews do zero
+=======
+Proejto do tabnews do zero
+>>>>>>> 747a3c6 (subindo /health)
---
+++ tests/unit/calculadora.test.js
@@ -0,0 +1,21 @@
+const calculadora = require("../../models/calculadora");
+
+test("Somar 2 + 2, deve retornar 4", () => {
+  const resultado = calculadora.somar(2, 2);
+  expect(resultado).toBe(4);
+});
+
+test("somar 5 + 100, deve retonar 105", () => {
+  const resultado = calculadora.somar(5, 100);
+  expect(resultado).toBe(105);
+});
+
+test("somar 'banana' + 100, deve retonar 'Erro'", () => {
+  const resultado = calculadora.somar("banana", 100);
+  expect(resultado).toBe("Erro");
+});
+
+test("Deve validar receber valores nulos ", () => {
+  const resultado = calculadora.somar();
+  expect(resultado).toBe("Erro");
+});
