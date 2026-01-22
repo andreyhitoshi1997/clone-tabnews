df314d8abff8779d11f5da5cf309f2b89b79b150
andreyhitoshi1997 <andreyhitoshi1997@gmail.com>
2026-01-21 23:21:15 -0300

    Subindo ultima aula

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -6,11 +6,21 @@ async function status(request, response) {
   const databaseVersionResult = await database.query("SHOW server_version;");
   const databaseVersionValue = databaseVersionResult.rows[0].server_version;
 
+  const databaseMaxConnectionsResult = await database.query("SHOW max_connections;");
+  const databaseMaxConnectionsValue = databaseMaxConnectionsResult.rows[0].max_connections;
+
+
+  //cast no banco de dados ::
+  const databaseOpenConnectionsResult = await database.query("SELECT count(*)::int FROM pg_stat_activity;");
+  const databaseOpenConnectionsValue = databaseOpenConnectionsResult.rows[0].count;
+
   response.status(200).json({
     updated_at: updatedAt,
     dependencies: {
       database: {
         version: databaseVersionValue,
+        max_connections: databaseMaxConnectionsValue,
+        open_connections: databaseOpenConnectionsValue
       },
     },
   });
